7bb15944072000b79e7a1d45a76de5b9db070a1e
tenel <ten.elena@outlook.com>
2020-05-13 22:44:26 +0200

    button to next page

--- ApBC/frontend/src/components/Intro.vue
+++ ApBC/frontend/src/components/Intro.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="hello">
         <div class="gameScreen">
-        <p>
-            Hej du lille vän!<br/>
+        <!--<p>
+            Hej vännen!<br/>
             Jag heter Po.<br/>
-            Kan du hjälpa mig och min kompis Leo <br/> att lära oss bokstäver och alfabetet?
-        </p>
-        <div class="img_intro"><img alt="Hej från Po" src="../assets/intropo.png" /></div>
-            <div class="button"><img alt="Next" src="../assets/go.png" /></div>
+            Kan du hjälpa mig <br/> och min kompis Leo <br/> att lära oss bokstäver <br/> och alfabetet?
+        </p>-->
+        <div class="img_intro"><img alt="Hej från Po" src="../assets/intro.png" class="intro"/></div>
+        <!--<div class="button"><img alt="Next" src="../assets/go.png" /></div>-->
+            <router-link to="/difficulty"><img alt="Next" src="../assets/go.png" /></router-link>
 </div></div>
 </template>
 
@@ -23,11 +24,14 @@
 </script>
 
 <style scoped>
-    /*@media only screen and (max-width: 870px) {*/
+    @media only screen and (max-width: 870px) {
         .gameScreen {
             display: grid;
+            grid-template-areas:
+                'main'
+                'down';
             grid-gap: 10px;
-            grid-auto-rows: minmax(100px, auto);
+            text-align: center;
         }
 
         p {
@@ -45,32 +49,33 @@
         }
 
         .img_intro {
-            width: 30%;
+            grid-area: main;
+        }
+        .intro {
+            width: 50%;
             height: auto;
-            margin-left: auto;
-            margin-right: auto;
             display: block;
             margin: 0 auto;
         }
 
         .img_intro:hover {
-            background-color: springgreen;
+            background-color: white;
         }
+
         .button:hover {
 
-    }
-    .button {
-        width: 50%;
-        height: auto;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-        margin: 0 auto;
-        font-family: "Sitka Text";
-        font-size: 20px;
-        color: darkgreen;
-        padding: 10px;
-        text-align: center;
-    }
+        }
 
+        .button {
+            grid-area: down;
+            width: 50%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+            font-size: 20px;
+            color: darkgreen;
+            padding: 10px;
+            text-align: center;
+        }
+    }
 </style>

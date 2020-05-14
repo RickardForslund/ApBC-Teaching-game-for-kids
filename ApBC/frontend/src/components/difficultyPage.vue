3c9c03cb73deafa3e578f79e78a9fa3f198cef9b
tenel <ten.elena@outlook.com>
2020-05-15 00:37:03 +0200

    level text adjust

--- ApBC/frontend/src/components/difficultyPage.vue
+++ ApBC/frontend/src/components/difficultyPage.vue
@@ -4,11 +4,11 @@
         <div class="img_choice"><img alt="Hur gammal är du?" src="../assets/choice.png" class="main_choice"/></div>
         <div class="options">
             <ul class="list">
-                <li class="small">
+                <li class="small">LITEN
                     <img alt="Liten" src="../assets/small.png" class="main_choice"/></li>
-                <li class="mellan"><router-link to="/spel"><img alt="Stor" src="../assets/medium.png" class="main_choice"/></router-link>
+                <li class="mellan">STOR<router-link to="/spel"><img alt="Stor" src="../assets/medium.png" class="main_choice"/></router-link>
                     </li>
-                <li class="stor">
+                <li class="stor">RIKTIGT STOR
                     <img alt="Riktigt stor" src="../assets/big.png" class="main_choice"/></li>
             </ul></div>
     </div>
@@ -51,8 +51,11 @@
         margin-bottom: 50px;
         margin-right: 20px;
         line-height: 20px;
-        display: inline;
+        display: inline-block;
         text-align: left;
+        font-size: 30px;
+        font-family: 'Patua One';
+        color: darkgreen;
     }
     .list {
         display: inline-block;

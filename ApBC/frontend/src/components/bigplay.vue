0a13c4d3980a9e3149b5eec81af227d40b19634f
Rickard <rickard.forslund@iths.se>
2020-06-02 10:59:29 +0200

    fixed grid

--- ApBC/frontend/src/components/bigplay.vue
+++ ApBC/frontend/src/components/bigplay.vue
@@ -234,7 +234,7 @@ margin-bottom: 90%;
 
     .scoreitems{
         display: grid;
-        grid-template-columns: repeat(5, 1fr);
+        grid-template-columns: repeat(4, 1fr);
         padding: 0px;
 
     }   

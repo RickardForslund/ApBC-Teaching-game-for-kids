a3fed5d2a96be5aaacabcfc9042cb680d1542aea
Rickard <rickard.forslund@iths.se>
2020-06-03 09:34:53 +0200

    grattis

--- ApBC/frontend/src/components/bigplay.vue
+++ ApBC/frontend/src/components/bigplay.vue
@@ -76,7 +76,7 @@
     data: function () {
         return {
             letters: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Å','Ä','Ö'],
-            answers: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Å','Ä','Ö'],
+            answers: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Å','Ä','Ö','Grattis'],
             sounds:[ "a.mp3", "b.mp3", "c.mp3", "d.mp3", "e.mp3", "f.mp3", "g.mp3", "h.mp3", "i.mp3", "j.mp3",
                      "k.mp3", "l.mp3", "m.mp3", "n.mp3", "o.mp3", "p.mp3", "q.mp3", "r.mp3", "s.mp3", "t.mp3",
                      "u.mp3","v.mp3","w.mp3","x.mp3", "y.mp3","z.mp3","å.mp3","ä.mp3","ö.mp3"],
